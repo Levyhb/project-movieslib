032d5718d88f862124ccefc125b01dc075fa6597
Levy Bezerra <levybholanda@gmail.com>
2022-09-05 23:04:14 -0300

    Colocando Carousel
    Os cards são exibidos em carousel agora.

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 import '../Styles/pages/MovieGrid.css';
+import arrow from "../img/arrow.png";
 
 const moviesUrl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 
 export default function Home() {
+  const carousel = useRef(null)
 
   const [topMovies, setTopMovies] = useState([])
 
   const getTopRatedMovies = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
-
     setTopMovies(data.results);
+  }
+
+  const handleLeftClick = (e) => {
+    e.preventDefault()
+    console.log(carousel.current.offsetWidth)
+    
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  }
+
+  const handleRightClick = (e) => {
+    e.preventDefault()
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
   }
 
   useEffect(() => {
@@ -26,13 +39,17 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="container">
+    <div className="container" >
       <h2 className="title">Top Rated movies:</h2>
-      <div className="movie-container">
+      <div className="movie-container" ref={carousel}>
       {topMovies.length === 0 && <Loading />}
       {topMovies && topMovies.map((movie) => (
         <MovieCard movie={ movie } key={ movie.id} />
       ))}
+      </div>
+      <div className="buttons">
+        <button className='arrow left'><img src={arrow} alt="arrow-icon" onClick={handleLeftClick}/></button>
+        <button className='arrow right'><img src={arrow} alt="arrow-icon" onClick={handleRightClick}/></button>
       </div>
     </div>
   )
