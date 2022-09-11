943cdac9e84770a11d7caf4d60e548cc7eb613b4
Levy Bezerra <levybholanda@gmail.com>
2022-09-10 21:10:03 -0300

    Update App.jsx

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,26 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Movie from './pages/MovieDetails'
-import NotFound from './pages/NotFound'
-import Search from './pages/Search'
-
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movie from "./pages/MovieDetails";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 function App() {
-
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route exact path='movie/:id' element={<Movie />}/>
-        <Route path='search' element={<Search />}/>
-        <Route path='*' element={<NotFound />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="movie/:id" element={<Movie />} />
+        <Route path="search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
