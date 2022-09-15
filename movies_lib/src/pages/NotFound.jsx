1ce6e94e2f7e82b4be4d65a29c98e784656e8a00
Levy Bezerra <levybholanda@gmail.com>
2022-09-15 19:51:24 -0300

    ReDeploy

--- movies_lib/src/pages/NotFound.jsx
+++ movies_lib/src/pages/NotFound.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Home from "./Home";
 import "../Styles/pages/NotFound.css";
 import johnTravoltaGif from "../img/john-travolta-GIF.gif";
 import { FaHome } from "react-icons/fa";
