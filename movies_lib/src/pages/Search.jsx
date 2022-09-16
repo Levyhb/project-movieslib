7bd59e539efa32a66d155f1b7e87969e9417ba7f
Levy Bezerra <levybholanda@gmail.com>
2022-09-16 20:13:48 -0300

    Adicionando tela de não encontrado em buscas por filmes inexistentes

--- movies_lib/src/pages/Search.jsx
+++ movies_lib/src/pages/Search.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { Link, useSearchParams } from "react-router-dom";
+import johnTravoltaGif from "../img/john-travolta-GIF.gif";
 import Loading from "../components/Loading";
 import MovieCard from "../components/MovieCard";
 import "../Styles/pages/MovieGrid.css";
+
+
 
 const searchUrl = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -11,13 +15,16 @@ export default function Search() {
   const [searchParams] = useSearchParams();
 
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const query = searchParams.get(`q`);
 
   const getSearchedMovies = async (url) => {
+    setLoading(true)
     const response = await fetch(url);
     const data = await response.json();
-
     setMovies(data.results);
+    setLoading(false)
   };
 
   useEffect(() => {
@@ -31,7 +38,18 @@ export default function Search() {
         Results for: <span className="query-text">{query}</span>
       </h2>
       <div className="container-grid">
-        {movies.length === 0 && <Loading />}
+        {loading && <Loading />}
+        {movies.length === 0 && (
+          <div className="not-found-page">
+            <div className="error-message">
+              <h2>Nenhum filme encontrado</h2>
+            </div>
+          <Link to="/">
+            Voltar para Home <FaHome />
+          </Link>
+          <img src={johnTravoltaGif} alt="john travolta gif" />
+        </div>
+        )}
         {movies &&
           movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
       </div>
